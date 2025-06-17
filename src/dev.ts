37a8c49a756048a9283a442d24aa0ba2757d9f9d
Michael Kreil <github@michael-kreil.de>
2025-06-17 11:52:19 +0200

    ci: try screenshots

--- src/dev.ts
+++ src/dev.ts
@@ -6,7 +6,7 @@ build(true);
 
 const fsRoot = (new URL('../docs', import.meta.url)).pathname;
 
-Deno.serve({ port: 8080 }, (req: Request) => {
+const server = Deno.serve({ port: 8080 }, (req: Request) => {
 	if (req.method !== 'GET') return ignore();
 	if (/\.icp$/.test(req.url)) return ignore();
 
@@ -16,3 +16,5 @@ Deno.serve({ port: 8080 }, (req: Request) => {
 		return new Response('ignore', { status: 404 });
 	}
 });
+
+export default server;
---
+++ src/screenshots.ts
@@ -0,0 +1,35 @@
+import toc from '../playground/toc.ts';
+import server from './dev.ts';
+import puppeteer from 'npm:puppeteer';
+
+const browser = await puppeteer.launch({
+	headless: true,
+	defaultViewport: { width: 1000, height: 1000, deviceScaleFactor: 2 },
+});
+const page = await browser.newPage();
+
+for (const entry of toc) {
+	for (const example of entry.examples) {
+		console.log(`Screenshot for ${example}`);
+		await page.goto(`http://localhost:8080/${example}/`);
+
+		const iframe = await page.$('#livecodes');
+		if (!iframe) throw new Error(`No iframe found for ${example}`);
+		iframe.scrollIntoView();
+		await new Promise((r) => setTimeout(r, 5000));
+
+		const clip = await iframe.boundingBox();
+		if (!clip) throw new Error(`No bounding box found for ${example}`);
+		const buffer = await page.screenshot({
+			type: 'png',
+			encoding: 'binary',
+			clip,
+			captureBeyondViewport: false,
+		}) as Uint8Array;
+		await Deno.writeFile(`./playground/${example}/preview.png`, buffer);
+	}
+}
+
+await page.close();
+await browser.close();
+await server.shutdown();
