2db45e8ec8399d22ebdbd26d8c0d7284ca39a926
Michael Kreil <github@michael-kreil.de>
2025-06-17 12:20:08 +0200

    add previews to index

--- src/screenshots.ts
+++ src/screenshots.ts
@@ -4,7 +4,7 @@ import puppeteer from 'npm:puppeteer';
 
 const browser = await puppeteer.launch({
 	headless: true,
-	defaultViewport: { width: 1000, height: 1000, deviceScaleFactor: 2 },
+	defaultViewport: { width: 1000, height: 1000, deviceScaleFactor: 1 },
 });
 const page = await browser.newPage();
 
